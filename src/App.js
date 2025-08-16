70e67c0ef3d9d3ac51ca21a6dad5661caa3e5016
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:24 +0530

    Initial commit: Weather-Dashboard

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Weather-Dashboard"</h1>
+        <p>"Beautiful weather application"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
